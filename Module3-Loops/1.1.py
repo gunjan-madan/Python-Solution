c69c9351964dbeb8ae43a7c01401f294695c5c4a
gunjan-madan <53206795+gunjan-madan@users.noreply.github.com>
2021-06-24 06:36:47 +0530

    Create 1.1.py

---
+++ Module3-Loops/1.1.py
@@ -0,0 +1,23 @@
+# Sam, your friend wants to print his dog's name,Snoopy, five times.
+# He asks you to help him, as you are learning to code in Python
+# Can you help him?
+'''Task 1: Repetition'''
+print("**** Task 1: ****")
+print()
+# One of the ways is to use 5 print statements like this:
+print ("Snoopy")
+print ("Snoopy") 
+print ("Snoopy")
+print ("Snoopy")
+print ("Snoopy")
+#Better way is to use while loop
+# The structure of the while loop is:
+''' while <condition>:
+      code to be repeated
+      code to be repeated
+    Code outside the loop '''
+# Uncomment the next 4 lines to see the while loop in:
+#counter = 1      	         # initialize a counter variable
+#while counter <= 5:       # loop condition
+ #print("Snoopy")  	   # code to execute
+ #counter = counter + 1
